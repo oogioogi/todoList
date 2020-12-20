afdf4d79a1c422d9182e0583229599e77b50b248
oogioogi <detacl73@gmail.com>
2020-12-20 13:12:53 +0900

    Initial Commit

---
+++ todoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoList
+//
+//  Created by 이용석 on 2020/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
